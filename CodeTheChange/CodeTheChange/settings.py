6a18889824487c91007d489a5ab14a93179347bd
TeachMeTheWays <teachmetheways652@gmail.com>
2023-04-14 22:25:51 -0700

    added test image

--- CodeTheChange/CodeTheChange/settings.py
+++ CodeTheChange/CodeTheChange/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "Website.apps.WebsiteConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
